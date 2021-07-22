559490d714be1f2993d9a976d6ffdc192227158c
Chris Cynarski <chris@cynarski.net>
2021-07-22 14:25:33 +0100

    Basic test for `jsonable` and `stringable`

--- tests/IofHelperTest.php
+++ tests/IofHelperTest.php
@@ -13,7 +13,10 @@ class IofHelperTest extends TestCase
         $i = new ArrayableTestDummy0();
         $this->assertFalse(Iof::arrayable($i));
 
-        $i = ['test'=>'test'];
+        $i = ['test' => 'test'];
+        $this->assertFalse(Iof::arrayable($i));
+
+        $i = 'Arrayable?';
         $this->assertFalse(Iof::arrayable($i));
 
         $i = new stdClass();
@@ -27,6 +30,45 @@ class IofHelperTest extends TestCase
     {
         $i = new ArrayableTestDummy1();
         $this->assertTrue(Iof::arrayable($i));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_not_be_jsonable(): void
+    {
+        $i = null;
+        $this->assertFalse(Iof::jsonable($i));
+
+        $i = 'Jsonable?';
+        $this->assertFalse(Iof::jsonable($i));
+
+        $i = ['test' => 'test'];
+        $this->assertFalse(Iof::jsonable($i));
+
+        $i = new stdClass();
+        $this->assertFalse(Iof::jsonable($i));
+
+        $i = new ArrayableTestDummy1();
+        $this->assertFalse(Iof::jsonable($i));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_be_jsonable(): void
+    {
+        $i = new JsonableTestDummy0();
+        $this->assertTrue(Iof::jsonable($i));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_be_stringable(): void
+    {
+        $i = new StringableTestDummy0();
+        $this->assertTrue(Iof::stringable($i));
     }
 }
 
@@ -47,5 +89,21 @@ class ArrayableTestDummy1
             'a' => $this->a,
             'b' => $this->b,
         ];
+    }
+}
+
+class JsonableTestDummy0 extends ArrayableTestDummy1
+{
+    public function toJson()
+    {
+        return json_encode($this->toArray());
+    }
+}
+
+class StringableTestDummy0 extends JsonableTestDummy0
+{
+    public function __toString(): string
+    {
+        return $this->toJson();
     }
 }
